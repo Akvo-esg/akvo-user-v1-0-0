e7f85da9082832140909ef38185485487c567fb3
Carlos Afonso <carlos.flach15@gmail.com>
2022-07-26 08:32:37 -0300

    feat: adciona background color para comentário header.

--- src/components/formComponets/Comentarios.jsx
+++ src/components/formComponets/Comentarios.jsx
@@ -1,25 +1,11 @@
 import React, { useRef, useState } from "react";
 import styles from '../../../styles/Comentario.module.scss'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import OverlayTrigger from "react-bootstrap/OverlayTrigger";
-// import Popover from "react-bootstrap/Popover";
-// import Overlay from 'react-bootstrap/Overlay';
 import Popover from '@mui/material/Popover';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import { faComment } from "@fortawesome/free-solid-svg-icons";
 
 function Comentarios(props) {
-  // // Handle Popover
-  // const [show, setShow] = useState(false);
-  // const [target, setTarget] = useState(null);
-  // const ref = useRef(null);
-
-  // const handleClick = (event) => {
-  //   setShow(!show);
-  //   setTarget(event.target);
-  // };
-
   const [anchorEl, setAnchorEl] = useState(null);
 
   const handleClick = (event) => {
@@ -63,31 +49,6 @@ function Comentarios(props) {
         <Typography sx={{ p: 1.5 }}>{props.comentario}</Typography>
       </Popover>
     </div>
-    // <div ref={ref}>
-    //   <span type="button" onClick={handleClick} className="position-relative">
-    //     <FontAwesomeIcon icon={faComment} />
-    //     <span
-    //       className="notificationSign fadeItem"
-    //       style={{ pointerEvents: "none" }}
-    //     >
-    //       <span className="visually-hidden">New alerts</span>
-    //     </span>
-    //   </span>
-    //   <Overlay
-    //     show={show}
-    //     target={target}
-    //     placement="bottom"
-    //     container={ref}
-    //     containerPadding={20}
-    //     rootClose
-    //     onHide={() => setShow(false)}
-    //   >
-    //     <Popover id="popover-contained">
-    //       <Popover.Header as="h3">Comentário:</Popover.Header>
-    //       <Popover.Body>{props.comentario}</Popover.Body>
-    //     </Popover>
-    //   </Overlay>
-    // </div>
   );
 }
 
